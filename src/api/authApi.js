7db5c02ac6659ddb7ffe0a86a68966b113c49714
Semen Kolyvanov <gdasdsadasdsa@gmail.com>
2025-05-05 12:51:23 +0500

    test

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,13 +2,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/server' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5050' }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
-        url: '/login',
+        url: '/auth',
         method: 'POST',
-        body: credentials
+        body: credentials,
+        // mode: 'no-cors',
+        responseHandler: (response) => response.text()
       })
     })
   })
